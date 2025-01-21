0ac28642a8a7b4d5fab582c2679a1c3521847032
chiemezie1 <chiemezieagbo1@gmail.com>
2025-01-21 09:42:04 +0100

    update project

--- src/components/TechnicalWriting/TechnicalWritingFile.tsx
+++ src/components/TechnicalWriting/TechnicalWritingFile.tsx
@@ -3,73 +3,73 @@ import { BlogPost } from '../types';
 const blogPosts: BlogPost[] = [
     {
         id: 1,
-        title: "Sentiment Analysis using Python",
-        description: "This project demonstrates the automation of data extraction from online sources and subsequent sentiment analysis. The automated extraction facilitates various applications, empowering decision-makers in the entertainment industry to gauge audience reactions for content creation, marketing strategies, and audience engagement initiatives. Sentiment analysis offers a comprehensive understanding of user sentiment, aiding in brand perception analysis and product feedback analysis.",
-        date: "2024-01-01",
-        link: "https://medium.com/@chiemezieagbo1/sentiment-analysis-using-python-automated-imdb-movie-review-extraction-and-analysis-a9f601fb79e6"
+        title: "Abstraction Using EIP-4337",
+        description: "This comprehensive overview of Ethereum account abstraction explains the concept, its importance, and provides a practical guide to implementing it using EIP-4337. It covers the fundamental concepts and includes a basic implementation example using the Ethereum Foundation's code.",
+        date: "2024-12-05",
+        link: "https://medium.com/@chiemezieagbo1/in-2021-stefan-thomas-lost-access-to-220-million-worth-of-bitcoin-because-he-couldnt-remember-3fa445e9d29f"
     },
     {
         id: 2,
-        title: "Python's Integer Object Caching: A Closer Look at Memory Optimization",
-        description: "Python's simplicity and readability are augmented by its unique feature: everything is an object. This project delves into Python object caching, focusing on memory optimization for integer objects. Python's object caching, especially for integers, reflects the language's commitment to efficient memory management, optimizing performance, and reducing memory overhead.",
-        date: "2024-01-31",
-        link: "https://medium.com/@chiemezieagbo1/pythons-integer-object-caching-a-closer-look-at-memory-optimization-4555d07b955f"
-    },
-    {
-        id: 3,
-        title: "Understanding Shebang Lines in Bash Scripts",
-        description: "The write-up explains shebang lines (hashbang or interpreter directives) in shell scripts, emphasizing their importance in specifying the interpreter to use when executing the script. While #!/bin/bash is common, #!/usr/bin/env bash offers portability, environment independence, and ease of maintenance advantages. Understanding shebang lines and alternative approaches enhances the creation of robust and adaptable scripts for various systems and environments.",
-        date: "2024-03-18",
-        link: "https://medium.com/@chiemezieagbo1/understanding-shebang-lines-in-bash-scripts-86f8018d6933"
-    },
-    {
-        id: 4,
-        title: "Aptos: A New Era in Blockchain Technology",
-        description: "This write-up introduces Aptos, a layer 1 blockchain focusing on scalability, speed, and user experience, revolutionizing decentralized applications (dApps). Aptos, inspired by the Diem project by Meta, boasts features like a parallel execution engine for processing thousands of transactions per second, minimizing transaction fees, and enhancing security. These features position Aptos at the forefront of the next generation of blockchain platforms, particularly beneficial for eCommerce applications.",
-        date: "2024-03-20",
-        link: "https://medium.com/@chiemezieagbo1/aptos-a-new-era-in-blockchain-technology-a7b97f8019cc"
-    },
-    {
-        id: 5,
-        title: "An Introduction to USDC and Circle's Web3 Services with a Focus on Programmable Wallets",
-        description: "The write-up details the features and benefits of Circle's Web3 services and USDC. It clearly explains how these services can transform digital finance by providing stability, reducing transaction costs, and simplifying cross-border payments.",
-        date: "2024-04-26",
-        link: "https://medium.com/@chiemezieagbo1/an-introduction-to-usdc-and-circles-web3-services-with-a-focus-on-programmable-wallets-c906bdb0eadb"
-    },
-    {
-        id: 6,
-        title: "Mental Health Assistant Chatbot with Sentiment-Based Response Generation",
-        description: "A Jupyter Notebook technical write-up on the Mental Health Assistant Chatbot designed to provide empathetic support based on user input. It uses sentiment analysis to assess emotional tone and intent classification to understand user needs, such as seeing a doctor or feeling unwell.",
-        date: "2024-10-30",
-        link: "https://colab.research.google.com/drive/1EZi_3-2Gerrp2WVSyWx_J1-P11pvoWeh?usp=sharing"
-    },
-    {
-        id: 7,
-        title: "What happens when you type google.com in your browser and press Enter",
-        description: "This explains what happens when you type a website address like 'www.google.com' and press enter. It provides a detailed behind-the-scenes process involved in getting the content you are looking for.",
-        date: "2024-07-18",
-        link: "https://medium.com/@chiemezieagbo1/what-happens-when-you-type-google-com-in-your-browser-and-press-enter-bf86d8f87e48"
-    },
-    {
-        id: 8,
-        title: "What's the point in having www in a URL?",
-        description: "This article explains the purpose of 'www' in a URL and why it's commonly seen in web addresses. It explores the technical background and whether 'www' is still necessary in modern web development.",
-        date: "2024-05-31",
-        link: "https://medium.com/@chiemezieagbo1/whats-the-point-in-having-www-in-a-url-3d84c9e68596"
-    },
-    {
-        id: 9,
         title: "Prove Online Facts Without Revealing Too Much",
         description: "This article explores TLSNotary, a tool that allows users to generate verifiable, cryptographic proofs of online interactions while revealing only the information they choose. It demonstrates how to prove details about social media accounts without exposing sensitive information.",
         date: "2024-11-25",
         link: "https://medium.com/@chiemezieagbo1/prove-online-facts-without-revealing-too-much-9c3262624064"
     },
     {
+        id: 3,
+        title: "What's the point in having www in a URL?",
+        description: "This article explains the purpose of 'www' in a URL and why it's commonly seen in web addresses. It explores the technical background and whether 'www' is still necessary in modern web development.",
+        date: "2024-05-31",
+        link: "https://medium.com/@chiemezieagbo1/whats-the-point-in-having-www-in-a-url-3d84c9e68596"
+    },
+    {
+        id: 4,
+        title: "What happens when you type google.com in your browser and press Enter",
+        description: "This explains what happens when you type a website address like 'www.google.com' and press enter. It provides a detailed behind-the-scenes process involved in getting the content you are looking for.",
+        date: "2024-07-18",
+        link: "https://medium.com/@chiemezieagbo1/what-happens-when-you-type-google-com-in-your-browser-and-press-enter-bf86d8f87e48"
+    },
+    {
+        id: 5,
+        title: "Mental Health Assistant Chatbot with Sentiment-Based Response Generation",
+        description: "A Jupyter Notebook technical write-up on the Mental Health Assistant Chatbot designed to provide empathetic support based on user input. It uses sentiment analysis to assess emotional tone and intent classification to understand user needs, such as seeing a doctor or feeling unwell.",
+        date: "2024-10-30",
+        link: "https://colab.research.google.com/drive/1EZi_3-2Gerrp2WVSyWx_J1-P11pvoWeh?usp=sharing"
+    },
+    {
+        id: 6,
+        title: "An Introduction to USDC and Circle's Web3 Services with a Focus on Programmable Wallets",
+        description: "The write-up details the features and benefits of Circle's Web3 services and USDC. It clearly explains how these services can transform digital finance by providing stability, reducing transaction costs, and simplifying cross-border payments.",
+        date: "2024-04-26",
+        link: "https://medium.com/@chiemezieagbo1/an-introduction-to-usdc-and-circles-web3-services-with-a-focus-on-programmable-wallets-c906bdb0eadb"
+    },
+    {
+        id: 7,
+        title: "Aptos: A New Era in Blockchain Technology",
+        description: "This write-up introduces Aptos, a layer 1 blockchain focusing on scalability, speed, and user experience, revolutionizing decentralized applications (dApps). Aptos, inspired by the Diem project by Meta, boasts features like a parallel execution engine for processing thousands of transactions per second, minimizing transaction fees, and enhancing security. These features position Aptos at the forefront of the next generation of blockchain platforms, particularly beneficial for eCommerce applications.",
+        date: "2024-03-20",
+        link: "https://medium.com/@chiemezieagbo1/aptos-a-new-era-in-blockchain-technology-a7b97f8019cc"
+    },
+    {
+        id: 8,
+        title: "Understanding Shebang Lines in Bash Scripts",
+        description: "The write-up explains shebang lines (hashbang or interpreter directives) in shell scripts, emphasizing their importance in specifying the interpreter to use when executing the script. While #!/bin/bash is common, #!/usr/bin/env bash offers portability, environment independence, and ease of maintenance advantages. Understanding shebang lines and alternative approaches enhances the creation of robust and adaptable scripts for various systems and environments.",
+        date: "2024-03-18",
+        link: "https://medium.com/@chiemezieagbo1/understanding-shebang-lines-in-bash-scripts-86f8018d6933"
+    },
+    {
+        id: 9,
+        title: "Python's Integer Object Caching: A Closer Look at Memory Optimization",
+        description: "Python's simplicity and readability are augmented by its unique feature: everything is an object. This project delves into Python object caching, focusing on memory optimization for integer objects. Python's object caching, especially for integers, reflects the language's commitment to efficient memory management, optimizing performance, and reducing memory overhead.",
+        date: "2024-01-31",
+        link: "https://medium.com/@chiemezieagbo1/pythons-integer-object-caching-a-closer-look-at-memory-optimization-4555d07b955f"
+    },
+    {
         id: 10,
-        title: "Abstraction Using EIP-4337",
-        description: "This comprehensive overview of Ethereum account abstraction explains the concept, its importance, and provides a practical guide to implementing it using EIP-4337. It covers the fundamental concepts and includes a basic implementation example using the Ethereum Foundation's code.",
-        date: "2024-12-05",
-        link: "https://medium.com/@chiemezieagbo1/in-2021-stefan-thomas-lost-access-to-220-million-worth-of-bitcoin-because-he-couldnt-remember-3fa445e9d29f"
+        title: "Sentiment Analysis using Python",
+        description: "This project demonstrates the automation of data extraction from online sources and subsequent sentiment analysis. The automated extraction facilitates various applications, empowering decision-makers in the entertainment industry to gauge audience reactions for content creation, marketing strategies, and audience engagement initiatives. Sentiment analysis offers a comprehensive understanding of user sentiment, aiding in brand perception analysis and product feedback analysis.",
+        date: "2024-01-01",
+        link: "https://medium.com/@chiemezieagbo1/sentiment-analysis-using-python-automated-imdb-movie-review-extraction-and-analysis-a9f601fb79e6"
     }
 ];
 
